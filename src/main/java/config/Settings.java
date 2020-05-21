b6172f376a79a0b6f0fe77786a49bf3ffc440a8e
mentselmiro <m.mentsel@gmail.com>
2020-05-21 16:36:27 +0300

    After Gradle and Chrome upgrade

--- src/main/java/config/Settings.java
+++ src/main/java/config/Settings.java
@@ -3,7 +3,7 @@ package config;
 public class Settings {
 
     public final static String CHROME_DRIVER_KEY = "webdriver.chrome.driver";
-    public final static String CHROME_DRIVER_PATH = "/home/mentsel/westernacher/chromedriver_linux641/chromedriver";
+    public final static String CHROME_DRIVER_PATH = "/home/mentsel/Drivers/chromedriver_linux64/chromedriver";
     public final static String WESTERNACHER_TEST_URL = "https://test.easyleave.de/";
 
 
